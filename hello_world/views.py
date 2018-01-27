452619ff95ca9357e4e50d2b52b945e9d5c94c62
JOGA <j.gaj@gmail.com>
2018-01-27 11:32:05 +0100

    zmiana

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
+moje_imie = "Natalia2"
 msg = "Hello World!"
 
 @app.route('/')
